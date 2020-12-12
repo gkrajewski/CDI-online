c8760f8759f6e9e2c0c8b2653333e7136d9c2bbf
pikrol <pkrol4478@gmail.com>
2020-12-12 12:11:02 +0100

    No automatic saving of csv.

--- server.R
+++ server.R
@@ -123,8 +123,8 @@ server <- function(input, output, session) {
       
       #Save answers and progress to csv file when session ended
       session$onSessionEnded(function() {
-        write.csv(answers, answersFile, row.names = F)
-        write.csv(progress, progressFile, row.names = F)
+        # write.csv(answers, answersFile, row.names = F)
+        # write.csv(progress, progressFile, row.names = F)
       })
       
     } else {
