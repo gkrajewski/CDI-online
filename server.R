11835affe79c721e833e3bda3de8940cd493e85d
pikrol <pkrol4478@gmail.com>
2021-03-03 17:17:26 +0100

    Change polish settings & fix some translations.

--- server.R
+++ server.R
@@ -13,8 +13,8 @@ server <- function(input, output, session) {
     } else {
       
       # updateQueryString(paste0("?id=", "IlYaL6gzKieyRx92YUl1q", "&form=", "wg", "&lang=", "pl"))
-      # updateQueryString(paste0("?id=", "test", "&form=", "ws", "&lang=", "pl"))
-      # session$reload()
+      updateQueryString(paste0("?id=", "test", "&form=", "ws", "&lang=", "pl"))
+      session$reload()
       output$sidebar <- renderText({"No needed URL parameters"})
       
     }
