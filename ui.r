08809566804862777a331941308019c7cf2a2a35
pikrol <pkrol4478@gmail.com>
2021-10-19 13:04:19 +0200

    Remove text about version

--- ui.r
+++ ui.r
@@ -10,7 +10,6 @@ ui <- fluidPage(
     tags$link(href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap", rel="stylesheet")
   ),
   
-  p(style = "text-align:right; font-size: 8px;", "Version BETA 3"),
   h1(textOutput("cdiNamePrefix")),
   h2(textOutput("cdiNameSufix")),
   uiOutput("menu"),
